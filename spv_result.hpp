24834751505bdb403f0e084f7f24d96135d40856
Alexander Ripar <alexander.ripar@gmail.com>
2022-03-29 14:22:11 +0200

    Move spvpcu::result enumeration into own header
    Also add new result enumerants for future use.

---
+++ spv_result.hpp
@@ -0,0 +1,29 @@
+#ifndef SPV_RESULT_HPP_INCLUDE_GUARD
+#define SPV_RESULT_HPP_INCLUDE_GUARD
+
+namespace spvcpu
+{
+	enum class result
+	{
+		success,
+		no_memory,
+		wrong_endianness,
+		wrong_magic,
+		invalid_spirv_version,
+		too_many_ids,
+		cannot_handle_header_schema,
+		shader_too_small,
+		shader_size_not_divisible_by_four,
+		shader_data_misaligned,
+		instruction_past_data_end,
+		unhandled_opcode,
+		instruction_wordcount_mismatch,
+		unknown_opcode,
+		too_many_instruction_args,
+		spirv_data_unknown_version,
+		spirv_data_enumeration_not_found,
+		unknown_argtype,
+	};
+}
+
+#endif // SPV_RESULT_HPP_INCLUDE_GUARD
